9730eb40845735f759cef98e0f1f65c6fab27927
Chao <wrongch@gmail.com>
2023-12-11 16:10:41 +0800

    增加：FeignClient的支持

--- src/main/java/io/github/newhoo/restkit/feature/javaimpl/spring/SpringControllerAnnotation.java
+++ src/main/java/io/github/newhoo/restkit/feature/javaimpl/spring/SpringControllerAnnotation.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum SpringControllerAnnotation {
 
     CONTROLLER("Controller", "org.springframework.stereotype.Controller"),
-    REST_CONTROLLER("RestController", "org.springframework.web.bind.annotation.RestController");
+    REST_CONTROLLER("RestController", "org.springframework.web.bind.annotation.RestController"),
+    FEIGN_CLIENT("FeignClient", "org.springframework.cloud.openfeign.FeignClient");
 
     private final String shortName;
     private final String qualifiedName;
